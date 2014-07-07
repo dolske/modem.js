2b51145bb0988c5065e04a9d066f2697a504a710
Justin Dolske <justin@dolske.net>
2014-07-07 11:12:25 -0700

    Add a random text button for fun.

---
+++ demo/lorem.js
@@ -0,0 +1,66 @@
+var LOREM_DATUM = [
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod " +
+  "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, " +
+  "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+
+  "Bacon ipsum dolor sit amet duis mollit pork belly turkey exercitation. Nulla " +
+  "doner nostrud flank quis ground round. Turducken est dolore mollit, ex sint " +
+  "drumstick aliquip tail sed id proident consequat prosciutto.",
+
+  "Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De " +
+  "carne lumbering animata corpora quaeritis. Summus brains sit, morbo vel " +
+  "maleficia? De apocalypsi gorger omero undead survivor dictum mauris.",
+
+  "Lorizzle ipsizzle dolizzle crunk daahng dawg, gangster adipiscing elit. " +
+  "Dizzle sapizzle yippiyo, check it out volutpat, suscipit mah nizzle, its fo " +
+  "rizzle vizzle, arcu. Pellentesque break it down tortor. Sizzle erizzle.",
+
+  "Pommy ipsum posh nosh farewell jellied eels Victoria sponge cake gallivanting " +
+  "around, bits 'n bobs jolly knows bugger all about nowt off with her head a " +
+  "right toff, one feels that mush bread and butter pudding porky-pies chin up " +
+  "utter shambles.",
+
+  "Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor, hodor, hodor hodor. " +
+  "Hodor hodor - hodor hodor hodor hodor hodor? Hodor hodor HODOR! Hodor HODOR " +
+  "hodor, hodor hodor, hodor. Hodor hodor?! Hodor hodor - hodor, hodor.",
+
+  "Haxx0r ipsum vi eaten by a grue echo finally race condition public mailbomb " +
+  "mainframe memory leak protocol int gcc I'm compiling float. Snarf cat machine " +
+  "code fopen ddos case daemon stack trace strlen system class buffer ssh headers " +
+  "todo script kiddies printf continue break.",
+
+  "Collaboratively administrate empowered markets via plug-and-play networks. " +
+  "Dynamically procrastinate B2C users after installed base benefits. " +
+  "Dramatically visualize customer directed convergence with revolutionary ROI.",
+
+  "Steamed roast flavour mocha shop galão espresso. Shop arabica a, medium, white " +
+  "coffee beans pumpkin spice extraction. Blue mountain macchiato cup chicory " +
+  "coffee affogato flavour skinny trifecta seasonal irish.",
+
+  "Back plato barleywine brew pitching dry craft infusion finishing wit squares. " +
+  "Bittering de scotch hops bright beer dopplebock conditioned pub fermentation " +
+  "glass mouthfeel anaerobic. Filter all-malt chocolate kettle ester hop sparge.",
+
+  "Cat ipsum dolor sit amet, hunt anything that moves or hopped up on goofballs " +
+  "hide when guests come over. Intrigued by the shower, or stare at ceiling climb " +
+  "leg for stretch and use lap as chair. Flop over, leave dead animals as gifts.",
+
+  "Ye ol' pirate ipsum, Sail ho scuppers Jolly Roger line skysail Smartly chantey " +
+  "parrel doubloon boatswain jack hail-shot. Tackle run a shot across the bow " +
+  "weigh anchor Jolly Roger scuttle case shot capstan. Bilge water rum, avast!",
+
+  "Sagan ipsum, finite but unbounded tendrils of gossamer clouds dispassionate " +
+  "extraterrestrial observer Orion's sword. As a patch of light star stuff " +
+  "harvesting star light, a mote of dust suspended in a sunbeam. Billions.",
+
+  "Galifreyan Raxacoricofallapatorius ninehundred  wibbly-wobbly timey-wimey The " +
+  "Silence is Coming! IT is the Doctor! Enemy of the Daleks. Would you like a " +
+  "jelly baby? MY VISION IS IMPAIRED! It's bigger on the inside! EXTERMINATE!",
+
+  "Cupcake ipsum dolor sit amet. Pastry chocolate toffee cotton candy. Cake " +
+  "biscuit lemon drops macaroon cheesecake cheesecake icing danish ice cream. " +
+  "Soufflé chupa chups gummies sugar plum jelly beans lemon drops. Muffins.",
+];
+
+function randomIpsum() {
+}
